c6d09932c73801d1a996e3bbd43e14a19d795646
deniskirbaba <kirbabadenis@gmail.com>
2025-08-29 13:02:57 +0300

    update preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,17 +13,29 @@ class NumericEncoder(TransformerMixin, BaseEstimator):
     - Apply np.log1p if required
     - Apply StandardScaler
     - Fill missing values
+    - Optionally, can random set values to a specific proportion of data
 
     Args:
         col_name (str): The name of the column to encode.
         apply_log (bool): Whether to apply np.log1p to the column.
         filling_value (float): The value to use for filling missing values.
+        random_state (int): Random seed for reproducibility.
+        random_sample_proportion (float): Proportion of data to randomly set to the filling value.
     """
 
-    def __init__(self, col_name: str, apply_log: bool, filling_value: float = -1.0):
+    def __init__(
+        self,
+        col_name: str,
+        apply_log: bool,
+        filling_value: float = -1.0,
+        random_state: int = 42,
+        random_sample_proportion: float = 0.0,
+    ):
         self.col_name = col_name
         self.apply_log = apply_log
         self.filling_value = filling_value
+        self.random_state = random_state
+        self.random_sample_proportion = random_sample_proportion
 
         self._scaler = StandardScaler()
         self._scaler.set_output(transform="pandas")
@@ -47,31 +59,41 @@ class NumericEncoder(TransformerMixin, BaseEstimator):
         X[[self.col_name]] = self._scaler.transform(X[[self.col_name]])
         X[[self.col_name]] = X[[self.col_name]].fillna(self.filling_value)
 
+        if self.random_sample_proportion > 0:
+            np.random.seed(self.random_state)
+            random_mask = np.random.rand(len(X)) < self.random_sample_proportion
+            X.loc[random_mask, self.col_name] = self.filling_value
+
         return X
 
 
 class CategoryEncoder(TransformerMixin, BaseEstimator):
     """
-    Class for category column preprocessing (Group rare categories and apply Binary Encoding).
+    Class for category column preprocessing:
+    - Use BinaryEncoder for categorical features.
+    - Randomly assigns a certain percentage of values to the category <NEW_{col_name}>.
 
     Args:
         col_name (str): The name of the column to encode.
-        min_freq (int): Categories which frequency is below this threshold will be put in f"<RARE_{self.col_name}>" category.
+        random_state (int): Random seed for reproducibility.
+        random_sample_proportion (float): Proportion of data to randomly assign to the <NEW_{col_name}> category.
     """
 
-    def __init__(self, col_name: str, min_freq: int):
+    def __init__(self, col_name: str, random_state: int, random_sample_proportion: float):
         self.col_name = col_name
-        self.min_freq = min_freq
+        self.random_state = random_state
+        self.random_sample_proportion = random_sample_proportion
 
-        self._encoder = BinaryEncoder(cols=[self.col_name], handle_missing="value", handle_unknown="value")
-        self._rare_col_name = f"<RARE_{self.col_name.upper()}>"
+        self._encoder = BinaryEncoder(cols=[self.col_name], handle_missing="value", handle_unknown=1)
+        self._new_cat_name = f"<NEW_{self.col_name.upper()}>"
 
-    def _group_rare_categories(self, series: pd.Series):
+    def _random_assign_new_category(self, series: pd.Series):
         """
-        Puts rare categories into a special category (check whether value is in self.frequent_categories).
+        Randomly assigns a certain percentage of values to the <NEW_{col_name}> category.
         """
-        mask = series.notna() & ~series.isin(self.frequent_categories_)
-        series.loc[mask] = self._rare_col_name
+        np.random.seed(self.random_state)
+        random_mask = np.random.rand(len(series)) < self.random_sample_proportion
+        series.loc[random_mask] = self._new_cat_name
 
         return series
 
@@ -82,10 +104,9 @@ class CategoryEncoder(TransformerMixin, BaseEstimator):
         X_one_col = X[[self.col_name]].copy()
         series = X_one_col[self.col_name]
 
-        value_counts = series.dropna().value_counts()
-        self.frequent_categories_ = value_counts.loc[lambda x: x >= self.min_freq].index.tolist()
+        X_one_col[self.col_name] = self._random_assign_new_category(series)
+        self.categories_ = X_one_col[self.col_name].unique().tolist()
 
-        X_one_col[self.col_name] = self._group_rare_categories(series)
         self._encoder = self._encoder.fit(X_one_col)
 
         return self
@@ -95,16 +116,25 @@ class CategoryEncoder(TransformerMixin, BaseEstimator):
         Applies Binary Encoding to the column.
         """
         X = X.copy()
-        X[self.col_name] = self._group_rare_categories(X[self.col_name].copy())
+        if hasattr(self, "categories_"):
+            X[self.col_name] = X[self.col_name].where(X[self.col_name].isin(self.categories_), self._new_cat_name)
 
-        return pd.merge(X, self._encoder.transform(X[[self.col_name]]), left_index=True, right_index=True)
+        return pd.merge(
+            X.drop(columns=[self.col_name]),
+            self._encoder.transform(X[[self.col_name]]),
+            left_index=True,
+            right_index=True,
+        )
 
 
 CATEGORIAL_PREP_PIPELINE = Pipeline(
     [
-        ("brand_name", CategoryEncoder("brand_name", min_freq=10)),
-        ("CommercialTypeName4", CategoryEncoder(col_name="CommercialTypeName4", min_freq=10)),
-        ("SellerID", CategoryEncoder(col_name="SellerID", min_freq=15)),
+        ("brand_name", CategoryEncoder("brand_name", random_state=42, random_sample_proportion=0.05)),
+        (
+            "CommercialTypeName4",
+            CategoryEncoder(col_name="CommercialTypeName4", random_state=42, random_sample_proportion=0.001),
+        ),
+        ("SellerID", CategoryEncoder(col_name="SellerID", random_state=42, random_sample_proportion=0.25)),
     ],
     verbose=True,
 )
@@ -120,7 +150,7 @@ NUMERIC_PREP_PIPELINE = Pipeline(
         ("comments_published_count", NumericEncoder("comments_published_count", apply_log=True)),
         ("photos_published_count", NumericEncoder("photos_published_count", apply_log=True)),
         ("videos_published_count", NumericEncoder("videos_published_count", apply_log=True)),
-        ("PriceDiscounted", NumericEncoder("PriceDiscounted", apply_log=False)),
+        ("PriceDiscounted", NumericEncoder("PriceDiscounted", apply_log=False, random_sample_proportion=0.0005)),
         ("item_time_alive", NumericEncoder("item_time_alive", apply_log=True)),
         ("item_count_fake_returns7", NumericEncoder("item_count_fake_returns7", apply_log=False)),
         ("item_count_fake_returns30", NumericEncoder("item_count_fake_returns30", apply_log=False)),
